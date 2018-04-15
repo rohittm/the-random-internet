e1503629d038da73821b499a5ff3f52814466349
Rohit Motwani <rohittmotwani@gmail.com>
2018-04-15 19:38:10 +0530

    Removed Service Worker Register

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'font-awesome/css/font-awesome.min.css';
 import './assets/css/style.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
